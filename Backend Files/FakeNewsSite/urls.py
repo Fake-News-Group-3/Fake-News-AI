a195715629ce953c5010ad7d148b050a6f0e57b2
Isaac Scarrott <36975191+isaacscarrott@users.noreply.github.com>
2018-04-16 11:56:13 +0100

    Version 2

--- Backend Files/FakeNewsSite/urls.py
+++ Backend Files/FakeNewsSite/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from . import views
 urlpatterns = [
-	path('', views.index)
+	path('', views.index),
+	path('About/', views.about)
 ]
 
 # Create your views here.
